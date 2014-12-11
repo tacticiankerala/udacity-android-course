8bea8a0715a7d06718ea358404b7144d49266a10
Sreenath N <tacticiankerala@gmail.com>
2014-12-11 14:17:40 +0530

    make network calls using volley

--- app/src/main/java/com/example/multunus/testapp/MainActivity.java
+++ app/src/main/java/com/example/multunus/testapp/MainActivity.java
@@ -1,12 +1,27 @@
 package com.example.multunus.testapp;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.text.format.Time;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -17,16 +32,55 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ArrayList forecast = new ArrayList<String>();
+        final ArrayList forecast = new ArrayList<String>();
         forecast.add("Today - 21");
         forecast.add("Tomorrow - 22");
         forecast.add("Thursday - 23");
         forecast.add("Friday - 24");
         forecast.add("Saturday - 25");
         forecast.add("Sunday - 26");
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,R.layout.list_item, R.id.listViewTextItem, forecast);
-        ListView listView = (ListView) this.findViewById(R.id.listView);
-        listView.setAdapter(arrayAdapter);
+
+        final ListView listView = (ListView) this.findViewById(R.id.listView);
+
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,R.layout.list_item, R.id.listViewTextItem, forecast);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=celsius&cnt=7";
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray list = response.getJSONArray("list");
+                            ArrayList temps = new ArrayList<String>();
+                            JSONObject item = (JSONObject) list.get(0);
+                            String temp = item.getJSONObject("temp").getString("day");
+                            String timeString = item.getString("dt");
+
+                            Time time = new Time();
+                            time.set(Long.parseLong(timeString) * 1000);
+                            temps.add(time.monthDay + " -- " + temp);
+                            arrayAdapter.addAll(temps);
+                            listView.setAdapter(arrayAdapter);
+                            listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+                                @Override
+                                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                    Toast.makeText(getApplicationContext(), "Hello Sreenath",
+                                            Toast.LENGTH_LONG).show();
+                                }
+                            });
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO Auto-generated method stub
+
+                    }
+                });
+        queue.add(jsObjRequest);
     }
 
 
